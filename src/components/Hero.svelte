fbd0a20a292794358c3b0a2afad365341c81cdc5
Rory Duncan <robust.rory@gmail.com>
2020-03-05 18:00:31 -0800

    Move hero content to component

---
+++ src/components/Hero.svelte
@@ -0,0 +1,29 @@
+<style>
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 800;
+    margin: 0 0 0.5rem 0;
+  }
+
+  .hero {
+    padding: 10vh 0;
+  }
+
+  @media (min-width: 56em) {
+    h1 {
+      font-size: 3.2em;
+    }
+
+    .hero {
+      padding: 25vh 0;
+    }
+  }
+</style>
+
+<section class="hero">
+  <h1>Fuse Fonts</h1>
+  <h2>Font Management for Adobe Photoshop</h2>
+</section>
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,31 +1,18 @@
 <style>
 
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 800;
-    margin: 0 0 0.5rem 0;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 3.2em;
-    }
-  }
 </style>
 
 <svelte:head>
   <title>Fuse Fonts</title>
 </svelte:head>
 
-<section class="hero">
-  <h1>Fuse Fonts</h1>
-  <h2>Font Management for Adobe Photoshop</h2>
-</section>
-
+<Hero />
 
 <section class="appeal">
   <p>This is the fuse fonts marketing website.</p>
 </section>
 
 
+<script>
+  import Hero from "../components/Hero.svelte";
+</script>
