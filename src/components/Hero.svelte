d915422dae53dc2656b4f4034b1e8e67925361ad
Rory Duncan <robust.rory@gmail.com>
2020-03-05 23:19:02 -0800

    Set h1 logo style

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,10 +1,26 @@
 <style>
-
   h1 {
     font-size: 2.8em;
     text-transform: uppercase;
     font-weight: 800;
     margin: 0 0 0.5rem 0;
+  }
+
+  .logo {
+    font-weight: 800;
+    text-transform: uppercase;
+    width: 100%;
+    margin-left: -0rem;
+  }
+
+  .logo::before {
+    content: "";
+    display: inline-block;
+    width: 0.7em;
+    height: 0.7em;
+    margin-right: 0.25em;
+    background-color: var(--accent-color);
+    border-radius: 2px;
   }
 
   .hero {
@@ -23,7 +39,6 @@
 </style>
 
 <section class="hero">
-  <h1>Fuse Fonts</h1>
-  <h2>Font Management for Adobe Photoshop</h2>
+  <h1 class="logo">Fuse Fonts</h1>
+  <h2>Organize your fonts into simple collections within Adobe Photoshop.</h2>
 </section>
-
