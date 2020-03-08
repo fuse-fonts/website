6abf0a2158124aa2a811a52daa7851d44bd9a4ed
Rory Duncan <robust.rory@gmail.com>
2020-03-07 21:58:18 -0800

    stylize hero actions

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -27,9 +27,54 @@
     padding: 10vh 0;
   }
 
+  .btn-link {
+    display: inline-block;
+    background: transparent;
+    border: 1px solid #b2b2b2;
+    padding: 0.5em 1.5em;
+    color: #ddd;
+    text-decoration: none;
+    font-size: 1em;
+    line-height: 1.6em;
+    cursor: pointer;
+    border-radius: 3px;
+  }
+
+  .btn-link:hover {
+    background-color: #332;
+  }
+
+  .btn-link + .btn-link {
+    margin-left: 0.5em;
+  }
+
+  .btn-link--buy {
+    padding: 0.5em 1em;
+    background: var(--accent-color);
+    color: #222;
+    border: 1px solid var(--accent-color);
+    border-radius: 3px;
+    transition: 0.1s ease-out all;
+    transform: scale(1);
+  }
+
+  .btn-link:active {
+    transform: scale(0.98);
+  }
+
+  .btn-link--buy:hover {
+    background-color: #ffd941;
+    color: #000;
+  }
+
+  .hero nav {
+    padding-top: 1em;
+    margin-top: 1em;
+  }
+
   @media (min-width: 56em) {
     h1 {
-      font-size: 3.2em;
+      font-size: 3.6em;
     }
 
     .hero {
@@ -42,8 +87,19 @@
   <h1 class="logo">Fuse Fonts</h1>
   <h2>Organize your fonts into simple collections within Adobe Photoshop.</h2>
   <nav class="hero-actions">
-    <a class="btn-link" href="#overview">Learn More</a>
-    <a class="btn-link" href="features">See Features</a>
-    <button class="btn-link">Buy</button>
+    <button class="btn-link btn-link--buy">Buy</button>
+    <button class="btn-link" on:click={learnMoreClicked}>Learn More</button>
+    <a class="btn-link" href="features">Features</a>
   </nav>
 </section>
+
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  function learnMoreClicked() {
+    dispatch("learn-more");
+  }
+
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
 <script>
   import Hero from "../components/Hero.svelte";
+
+  let overviewEl = null;
+
+  function scrollOverViewIntoView() {
+    if (overviewEl === null) return;
+
+    overviewEl.scrollIntoView({ behavior: "smooth" });
+  }
 </script>
 
 <style>
@@ -10,8 +18,8 @@
   <title>Fuse Fonts</title>
 </svelte:head>
 
-<Hero />
+<Hero on:learn-more={scrollOverViewIntoView} />
 
-<section class="overview" id="overview">
+<section class="overview" id="overview" bind:this={overviewEl}>
   <h2>How it works</h2>
 </section>
