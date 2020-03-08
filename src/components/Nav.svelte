be083adc03ae183eb9975f35f6f3525a2bceccfd
Rory Duncan <robust.rory@gmail.com>
2020-03-07 20:44:56 -0800

    Repurpose about.svelte -> learn.svelte

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -96,11 +96,6 @@
         Features
       </a>
     </li>
-    <!-- <li>
-      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
-        About
-      </a>
-    </li> -->
     <li>
       <a aria-current={segment === 'learn' ? 'page' : undefined} href="about">
         Learn
---
+++ src/routes/learn.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <title>Learn</title>
+</svelte:head>
+
+<h1>Learn</h1>
