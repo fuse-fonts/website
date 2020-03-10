6bc56799b2ac93fa61fd83bb04226b1a2adb9f6a
Rory Duncan <robust.rory@gmail.com>
2020-03-10 00:58:55 -0700

    refine footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,21 +1,25 @@
-
 <script>
   import Container from "./Container.svelte";
 
-  const newsletterSignupLink = "https://cdn.forms-content.sg-form.com/1e965d7f-62a1-11ea-b917-aa5a30c70ab5";
+  const newsletterSignupLink =
+    "https://cdn.forms-content.sg-form.com/1e965d7f-62a1-11ea-b917-aa5a30c70ab5";
 </script>
+
 <style>
   footer {
     background-color: #111;
-    padding: 4em 0;
-    text-transform: lowercase;
+    padding: 4rem 0;
+
+    font-size: 0.8em;
   }
 </style>
 
 <footer>
   <Container>
-     <nav>
-      <a href={newsletterSignupLink} target="_blank">subscribe by email</a>
-     </nav>
+    <nav>
+      <a href={newsletterSignupLink} target="_blank">
+        Subscribe to Fuse Fonts updates
+      </a>
+    </nav>
   </Container>
 </footer>
