c652f452dadb7bcee182ac25f902c015d050dd3d
Rory Duncan <robust.rory@gmail.com>
2020-03-11 23:37:10 -0700

    Boost font size

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -7,13 +7,19 @@
   .text-center {
     text-align: center;
   }
+
+  h1 {
+    font-size: 5em;
+  }
 </style>
 <Container>
 <div class="text-center">
   <h1>404</h1>
   <h2>That page is lost, and is now profound.</h2>
+  <p class="content-block">
+    <a href="." class="btn-link btn-link--buy">Return to Fuse Fonts</a>
+  </p>
 
-  <a href="." class="btn-link btn-link--buy">Return to Fuse Fonts</a>
 </div>
 
 
