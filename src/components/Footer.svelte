5c93fcf73dd07e72033ce68a747804dd782ab12f
Rory Duncan <robust.rory@gmail.com>
2020-03-13 01:15:36 -0700

    Add link to repo

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -12,7 +12,7 @@
 <style>
   footer {
     background-color: #111;
-    padding: 4rem 2em;
+    padding: 4rem 2em 0rem 2rem;
     font-size: 0.8em;
   }
 
@@ -34,38 +34,51 @@
     margin-top: 1em;
     text-align: center;
   }
+
+  .logo-container .open-source {
+    color: #bbb;
+    display: inline-block;
+    margin: 1em auto;
+  }
 </style>
 
 <footer>
 
-    <nav>
-      <div>
-        <h4>News</h4>
-        <p>
-          <a href={sendGridSignupURL} target="_blank">Email Updates</a>
-          <br />
-          <span class="muted">
-            Subscribe to Fuse Fonts product updates and special offers
-          </span>
-        </p>
-      </div>
-
-      <div>
-        <h4>Contact</h4>
-        <p>
-          <a href={`mailto:${contactEmailAddress}`} target="_blank">
-            {contactEmailAddress}
-          </a>
-          <br />
-          <a href={`mailto:${supportEmailAddress}`} target="_blank">
-            {supportEmailAddress}
-          </a>
-        </p>
-      </div>
-
-    </nav>
-    <div class="logo-container">
-      <Logo />
+  <nav>
+    <div>
+      <h4>News</h4>
+      <p>
+        <a href={sendGridSignupURL} target="_blank">Email Updates</a>
+        <br />
+        <span class="muted">
+          Subscribe to Fuse Fonts product updates and special offers
+        </span>
+      </p>
     </div>
+
+    <div>
+      <h4>Contact</h4>
+      <p>
+        <a href={`mailto:${contactEmailAddress}`} target="_blank">
+          {contactEmailAddress}
+        </a>
+        <br />
+        <a href={`mailto:${supportEmailAddress}`} target="_blank">
+          {supportEmailAddress}
+        </a>
+      </p>
+    </div>
+
+  </nav>
+
+  <div class="logo-container">
+    <Logo />
+    <p>
+      ❤
+      <a class="open-source" href="https://github.com/fuse-fonts/website" target="_blank">
+        Open Source
+      </a>
+    </p>
+  </div>
 
 </footer>
