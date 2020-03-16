4dbbd71998cc4295dea21dcf5ba46f066fcc8989
Rory Duncan <robust.rory@gmail.com>
2020-03-15 20:56:25 -0700

    Update CDN url

--- src/routes/_config.js
+++ src/routes/_config.js
@@ -2,6 +2,6 @@ export const contactEmailAddress = "hello@fusefonts.com";
 export const supportEmailAddress = "support@fusefonts.com";
 
 export const demoURL = "https://demo.fusefonts.com";
-export const cdnURL = "https://fuse-fonts-cdn.firebaseapp.com/learn/";
+export const cdnURL = "https://cdn.fusefonts.com";
 
 export const sendGridSignupURL = "https://cdn.forms-content.sg-form.com/1e965d7f-62a1-11ea-b917-aa5a30c70ab5";
