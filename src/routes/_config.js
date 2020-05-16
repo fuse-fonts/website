130c970f7815624ead21b6bbd8b898835ccaea19
Rory Duncan <robust.rory@gmail.com>
2020-05-16 14:56:04 -0700

    Add messenger url

--- src/routes/_config.js
+++ src/routes/_config.js
@@ -17,8 +17,10 @@ export const adobeExchangePrice = "4";
 export const facebookURL = "https://www.facebook.com/fusefonts/";
 export const instagramURL = "https://www.instagram.com/fusefonts/";
 export const githubURL = "https://github.com/fuse-fonts/";
+export const messengerURL = "https://m.me/fusefonts";
 
 export const storeURLs = {
   CREATIVE_MARKET: "https://creativemarket.com/fusefonts/4952681-Fuse-Fonts",
   ADOBE_EXCHANGE: "https://exchange.adobe.com/creativecloud.details.102714.html",
 };
+
